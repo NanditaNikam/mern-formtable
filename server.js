f10b28067177a879e6a6f9ec15f937f694853dcb
NanditaNikam <nandita.nikam_comp2020@pccoer.in>
2022-01-30 22:35:14 +0530

    third commit

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const exphbs = require('express-handlebars');
 const bodyparser = require('body-parser');
 
 const employeeController = require('./controllers/employeeController');
+const port = process.env.PORT || 3000;
 
 var app = express();
 app.use(bodyparser.urlencoded({
@@ -21,3 +22,4 @@ app.listen(3000, () => {
 });
 
 app.use('/employee', employeeController);
+
